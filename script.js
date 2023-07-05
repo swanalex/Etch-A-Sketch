44e4ee02bcae92eaf8fa43b5138be5456c3be952
Alex Swanson <swanalex@oregonstate.edu>
2023-07-05 14:36:05 -0400

    test more grid stuff

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-for (let i = 1; i<=256; i++) {
+for (let i = 1; i<=10000; i++) {
     const div = document.createElement('div');
     const container = document.querySelector('#container');
     div.classList.add('gridDiv')
     container.appendChild(div);
     
-    div.onmouseover = () => div.style.backgroundColor = 'red';
+    div.onmousedown = () => div.style.backgroundColor = 'red';
 
     if (i%16 === 0) {
         const div = document.createElement('div');
