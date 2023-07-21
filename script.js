103cb297517bcfedb6e7ccb8bb8ac41dcdda6269
Alex Swanson <swanalex@oregonstate.edu>
2023-07-21 16:32:21 -0400

    test javascript variables for box width and height

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-for (let i = 1; i<=10000; i++) {
+
+let boxes_per_row = 16;
+let board_size = boxes_per_row**2;
+let px_width = 25;
+let px_height = 25;
+
+for (let i = 1; i<=board_size; i++) {
     const div = document.createElement('div');
     const container = document.querySelector('#container');
-    div.classList.add('gridDiv')
+    div.classList.add('gridDiv');
     container.appendChild(div);
-    
-    div.onmousedown = () => div.style.backgroundColor = 'red';
 
-    if (i%16 === 0) {
+    div.style.width = px_width + 'px';
+    div.style.height = px_height + 'px';
+    div.onmouseover = () => div.style.backgroundColor = 'red';
+
+    if (i%boxes_per_row === 0) {
         const div = document.createElement('div');
         div.classList.add('break')
         container.appendChild(div);
